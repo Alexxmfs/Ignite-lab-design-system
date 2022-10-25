9a6a63cadc973eb55f0233963d83e88c2eb7bcf5
Alexxmfs <alexxmfs@hotmail.com>
2022-10-25 16:13:32 -0300

    QUACK

---
+++ storybook-static/assets/preview.19b5d3f4.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.cc808bcc.js";import"./iframe.442d8b84.js";import"./es.map.constructor.83c52b38.js";import"./es.number.to-fixed.9de0bc96.js";import"./index.eefbf568.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.19b5d3f4.js.map
