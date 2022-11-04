26f66b7b3fc93ffb03cc0124fff2d5afb088b8cc
Alexxmfs <alexxmfs@hotmail.com>
2022-11-04 14:59:41 -0300

    commit

---
+++ storybook-static/assets/preview.a2f602cc.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.ff433992.js";import"./iframe.b3dc7f6f.js";import"./es.map.constructor.12bdf190.js";import"./es.number.to-fixed.b9d6e275.js";import"./index.f2bf244c.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.a2f602cc.js.map
